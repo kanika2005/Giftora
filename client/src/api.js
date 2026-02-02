53ba71a8a7e215ab6dffed39358d7051194e6ba4
KANIKA <156302668+kanika2005@users.noreply.github.com>
2026-02-02 12:14:44 +0530

    Refactor API setup to use axios with credentials

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true
 });
+
+export default API;
